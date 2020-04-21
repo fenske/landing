9aa58b884ffe672502274a9f82ba6bde295ced21
Anton Fenske <anton@fenske.xyz>
2020-04-21 15:52:16 +0200

    WIP.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
-import Head from 'next/head'
-import Nav from '../components/nav'
+import Head from 'next/head';
+import Nav from '../components/nav';
+import { getSortedPostsData } from '../lib/posts';
 
-export default () => (
+export default ({ allPostsData }) => (
   <div>
     <Head>
       <title>CoreSkills</title>
@@ -11,6 +12,29 @@ export default () => (
     <Nav />
     <div className="hero">
       <h1 className="title">Next.js + Tailwind CSS</h1>
+      <section className="text-base p-1 justify-center">
+        <h2 className="text-lg">Blog</h2>
+        <ul className="">
+          {allPostsData.map(({ id, date, title }) => (
+            <li className="mt-6" key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   </div>
-)
+);
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    }
+  };
+}
